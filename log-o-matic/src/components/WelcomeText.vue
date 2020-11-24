6f2240a18ca6bbfad7f1f3a2d8c7b0a152a347d8
André Skv <andre.skvarc@elev.ga.ntig.se>
2020-11-24 09:21:04 +0100

    Fixed first page

---
+++ log-o-matic/src/components/WelcomeText.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+      <v-col class="mb-2">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Log-O-Matic
+        </h1>
+      </v-col>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Welcometext',
+
+    data: () => ({
+    //
+  }),
+    
+  })
+</script>
--- log-o-matic/src/views/Home.vue
+++ log-o-matic/src/views/Home.vue
@@ -7,11 +7,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Welcometext from '@/components/WelcomeText.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Welcometext,
   },
 })
 export default class Home extends Vue {}
