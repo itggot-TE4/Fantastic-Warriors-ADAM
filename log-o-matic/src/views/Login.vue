b1f7cba684fd35ea5140edddd3eb8871e9dbc2e4
André Skv <andre.skvarc@elev.ga.ntig.se>
2020-11-24 12:03:36 +0100

    Fixed structure

--- log-o-matic/src/views/Login.vue
+++ log-o-matic/src/views/Login.vue
@@ -1,16 +1,39 @@
 <template>
   <v-container>
-      <LoginForum/>
+       <div id="app">
+    <v-app id="inspire">
+      <v-card class="overflow-hidden" color="gray">
+        <v-toolbar flat color="gray">
+          <v-icon>mdi-account</v-icon>
+          <v-toolbar-title class="font-weight-light">
+            User Profile
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-text-field color="white" label="Name" v-model="name">
+          </v-text-field>
+          <v-text-field color="white" label="password" v-model="password">
+          </v-text-field>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" @click="signIn"> Sign in </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-app>
+  </div>
   </v-container>
 </template>
 
 <script lang="ts">
 
-import LoginForum from '@/components/LoginForum.vue';
+// import LoginForum from '@/components/LoginForum.vue';
 
 export default {
     components: {
-        LoginForum,
+        // LoginForum,
     }
 }
 </script>
