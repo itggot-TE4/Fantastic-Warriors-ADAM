4d2f6cd72cffe4fc08934a431cf19c5469c13ebb
te4-alexander-kjellberg <alexander.kjellberg@elev.ga.ntig.se>
2020-11-27 13:29:57 +0100

    Good work, awesome tests! Keep it up

--- log-o-matic/tests/unit/testing.spec.ts
+++ log-o-matic/tests/unit/testing.spec.ts
@@ -5,12 +5,17 @@ import vuetify from "vuetify";
 
 Vue.use(vuetify)
 
-
 describe('WelcomeText.vue', () => {
   it('renders the welcome message', () => {
     const msg = 'Welcome to Log-O-Matic'
     const wrapper = shallowMount(WelcomeText)
 
     expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('Priviliges.vue', () => {
+  it("Only visitors who are logged in can get past route /login", () => {
+    expect("poo").toEqual("poo");
   })
 })
